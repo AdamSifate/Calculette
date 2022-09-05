c74bedfa071e788ee71f2fbf9533a87c41703389
SIFATE Adam <Adam.SIFATE@cpnv.ch>
2022-09-05 08:50:11 +0200

    ReviewCode

---
+++ calculette/calculette/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace calculette
+{
+    public class Class1
+    {
+        public static int Add(int num1, int num2) {
+
+            return num1 + num2;
+        }
+
+
+        public static int Substractor(int num1, int num2) { 
+        
+            return num1 - num2;
+        }
+
+        public static int Multiply(int num1, int num2) { 
+            return num1 * num2;
+        }
+
+        public static int Divide(int num1, int num2) {
+
+            return num1 / num2;
+        }
+
+    }
+    
+    
+}
---
+++ calculette/calculette/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace calculette
+{ 
+    public class Test
+    {
+        public static void Main()
+        {
+            int i = 0;
+            do
+            {
+                System.Console.Clear();
+                calcul();
+                DisplayMessage("vous voulez continuer ? (1 pour arreter)(autre pour continuer)");
+                i = Convert.ToInt32(Console.ReadLine());
+            
+            } while (i != 1);   
+        }
+    
+        public static void calcul()
+        {
+
+            int num1 = AskUserIntegerInput("Num1");
+
+            int num2 = AskUserIntegerInput("Num2");
+        
+
+            switch (AskUserCharÏnput("+ Addition\n- Soustraction\n* Multiplication\n/ Division"))
+            {
+                case '+':
+
+                    
+                    DisplayResult(num1,'+',num2, Class1.Add(num1, num2));
+            
+                    break;
+                case '-':
+
+                    //DisplayResult(num1, '-', num2, Substractor(num1, num2));
+                    break;
+                case '*':
+
+                    //DisplayResult(num1, '*', num2, Multiply(num1, num2));
+                    break;
+                case '/':
+
+                    //DisplayResult(num1, '/', num2, Divide(num1, num2));
+                    break;
+                default:
+                    DisplayMessage("Invalid choice");
+                    break;
+            }
+        }
+
+       
+        
+        public static char AskUserCharÏnput(string question) 
+        {
+            DisplayMessage(question);
+            return Convert.ToChar(Console.ReadLine()); ;
+        }
+
+        public static int AskUserIntegerInput(string question)
+        {
+            DisplayMessage(question);
+        
+
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+
+        public static void DisplayResult(int op1, char oper, int op2, int result) 
+        {
+            DisplayMessage("Vous avez effectuer " + op1 + oper + op2 + " est = " + result);
+        }
+
+        public static void DisplayMessage(string message) 
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
